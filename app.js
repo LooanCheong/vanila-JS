29a5b01330b02563d689ae510436cb28b6641993
LooanCheong <jjyejun@naver.com>
2022-10-17 20:20:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const a = 5;
 const b = 2;
-
-const myName = "yejun";
+let myName = "yejun";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Yejun";
 console.log("hello " + myName);
