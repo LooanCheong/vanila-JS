3c31ab4126663a5104949dccfce8d6a0f3e94a41
LooanCheong <jjyejun@naver.com>
2022-10-27 17:55:11 +0900

    add am,pm

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,13 @@ function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}`;
+  let ampm = "";
+  if (date.getHours < 12) {
+    ampm = "am";
+  } else {
+    ampm = "pm";
+  }
+  clock.innerText = `${hours}:${minutes} ${ampm}`;
 }
 
 getClock();
