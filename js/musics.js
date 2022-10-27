224fc6f52ef3237fa4dbf50a7588f873d3756402
LooanCheong <jjyejun@naver.com>
2022-10-28 01:13:24 +0900

    Update musics.js

--- js/musics.js
+++ js/musics.js
@@ -1,43 +1,43 @@
 const musics = [
   {
-    music: "늦었다고 생각할 때가 늦은 거다.",
-    album: "test",
+    music: "Blueming",
+    album: "Love poem",
   },
   {
-    music: "포기하면 편하다.",
-    album: "test",
+    music: "Coin",
+    album: "LILAC",
   },
   {
-    music: "안되면 말고.",
-    album: "test",
+    music: "너랑 나",
+    album: "Last Fantasy",
   },
   {
-    music: "나까지 나설 필요 없다.",
-    album: "test",
+    music: "드라마",
+    album: "조각집",
   },
   {
-    music: "즐길 수 없으면 피하라.",
-    album: "test",
+    music: "하루 끝",
+    album: "스무 살의 봄",
   },
   {
-    music: "어려운 길은 길이 아니다.",
-    album: "test",
+    music: "스물셋",
+    album: "Smash Hits",
   },
   {
-    music: "티끌 모아 티끌.",
-    album: "test",
+    music: "이 지금",
+    album: "Palette",
   },
   {
-    music: "동정할 거면 돈으로 줘라.",
-    album: "test",
+    music: "팔레트",
+    album: "Palette",
   },
   {
-    music: "인생은 한방이 아니다.",
-    album: "test",
+    music: "좋은 날",
+    album: "Real",
   },
   {
-    music: "내일도 할 수 있는 일을 굳이 오늘 할 필요 없다.",
-    album: "test",
+    music: "strawberry moon",
+    album: "strawberry moon",
   },
 ];
 
