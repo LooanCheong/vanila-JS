842f80ed3da8a80115ea21387436e3e00606c578
LooanCheong <jjyejun@naver.com>
2022-10-28 00:36:55 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "✔";
+  button.innerText = "🗑";
   button.addEventListener("click", deleteToDo);
   li.appendChild(button);
   li.appendChild(span);
